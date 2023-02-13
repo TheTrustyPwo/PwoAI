d827887c647ca54d9b762f817b9d3027504f3863
TheTrustyPwo <shucecai@gmail.com>
2023-02-13 15:51:38 +0800

    Co-authored-by: TheTrustyPwo <thetrustypwo@gmail.com>

--- pwo.py
+++ pwo.py
@@ -2,8 +2,5 @@ import random
 
 while True:
     input()
-    x = print(random.choice(["yay", "lol", "wat", "funny", "er", ":D"]))
-    if (x == "yay"):
-        print("yntay")
-        #shuce is gay
+    print(random.choice(["yay", "lol", "wat", "funny", "er", ":D"]))
 
