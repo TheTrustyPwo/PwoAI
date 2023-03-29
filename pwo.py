9d2c80a72d5b4527712344b4cc72f8ead160230e
TheTrustyPwo <shucecai@gmail.com>
2023-03-29 20:55:29 +0800

    Made changes

--- pwo.py
+++ pwo.py
@@ -2,5 +2,5 @@ import random
 
 while True:
     input("Write something:")
-    print(random.choice(["yay", "lol", "wat", "funny", "er", ":D", "die"]))
+    print(random.choice(["yay", "lol", "wat", "funny", "er", ":D", "die", "ok"]))
 
